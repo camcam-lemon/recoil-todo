9d7de2259b3438617773c3bbd4229e9b822f27da
camcam-lemon <omega.camcamlemon@gmail.com>
2020-03-17 00:21:03 +0900

    :syringe: tsxファイルのreact/prop-typesを除外した

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,21 @@ const WARNING = 1;
 const ERROR = 2;
 
 // airbnbルールでfor...ofのみ許可
-const NO_RESTRICTED_SYNTAX_OPTION= [
+const NO_RESTRICTED_SYNTAX_OPTION = [
     {
         selector: 'ForInStatement',
-        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+        message:
+            'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
     },
     {
         selector: 'LabeledStatement',
-        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+        message:
+            'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
     },
     {
         selector: 'WithStatement',
-        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+        message:
+            '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
     },
 ];
 
@@ -22,12 +25,12 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     extends: [
         'airbnb',
+        'airbnb/hooks',
         'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended',
         'prettier/@typescript-eslint',
         'prettier/react',
     ],
-    plugins: ['react-hooks'],
     parserOptions: {
         sourceType: 'module',
         ecmaFeatures: {
@@ -52,29 +55,46 @@ module.exports = {
             },
         },
     },
+    overrides: [
+        {
+            files: ['**/*.tsx'],
+            rules: {
+                'react/prop-types': 'off',
+            },
+        },
+    ],
     rules: {
-        'prettier/prettier': [ERROR, {
-            tabWidth: 4,
-            singleQuote: true,
-            printWidth: 100,
-            trailingComma: "all",
-        }],
+        'prettier/prettier': [
+            ERROR,
+            {
+                tabWidth: 4,
+                singleQuote: true,
+                printWidth: 100,
+                trailingComma: 'all',
+            },
+        ],
         'import/no-cycle': OFF,
         'no-plusplus': OFF,
         '@typescript-eslint/explicit-function-return-type': OFF,
         '@typescript-eslint/no-explicit-any': OFF,
         '@typescript-eslint/prefer-interface': OFF,
-        '@typescript-eslint/no-unused-vars': [ERROR, {
-            varsIgnorePattern: '^_',
-            args: 'after-used',
-            ignoreRestSiblings: true,
-            caughtErrorsIgnorePattern: "e|err|error"
-        }],
-        '@typescript-eslint/no-use-before-define': [ERROR, {
-            typedefs: false // ESLint側が対応されていなくて動かない
-        }],
+        '@typescript-eslint/no-unused-vars': [
+            ERROR,
+            {
+                varsIgnorePattern: '^_',
+                args: 'after-used',
+                ignoreRestSiblings: true,
+                caughtErrorsIgnorePattern: 'e|err|error',
+            },
+        ],
+        '@typescript-eslint/no-use-before-define': [
+            ERROR,
+            {
+                typedefs: false, // ESLint側が対応されていなくて動かない
+            },
+        ],
         'max-len': [ERROR, 120],
-        'curly': [ERROR, 'multi-or-nest'],
+        curly: [ERROR, 'multi-or-nest'],
         'react-hooks/rules-of-hooks': ERROR,
         'react-hooks/exhaustive-deps': WARNING,
         'react/jsx-filename-extension': [
